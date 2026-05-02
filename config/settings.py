ee92c4bf6e934f1145227da30f2cc9d02d67acda
Pedro Perez <pedroperez.arr@gmail.com>
2026-05-02 13:06:27 -0300

    usar static para media en produccion

--- config/settings.py
+++ config/settings.py
@@ -147,6 +147,3 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MEDIA_URL = '/static/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
