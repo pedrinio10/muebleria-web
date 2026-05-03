8a273d9f299ec094a6fa8828fa557bdfff92e53a
Pedro Perez <pedroperez.arr@gmail.com>
2026-05-02 21:40:41 -0300

    agregar dominio propio a allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = [
+    'muebleria-web.onrender.com',
+    'www.muebleriatorres.com',
+    'muebleriatorres.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 STATIC_URL = '/static/'
