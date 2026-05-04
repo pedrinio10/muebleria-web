dddabeab4730726993c6c632d39bf63e3ed26e64
Pedro Perez <pedroperez.arr@gmail.com>
2026-05-04 20:15:04 -0300

    configurar cloudinary para imagenes

--- config/settings.py
+++ config/settings.py
@@ -64,7 +64,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tienda',
     'import_export',
+    'cloudinary',
+    'cloudinary_storage',
 ]
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
